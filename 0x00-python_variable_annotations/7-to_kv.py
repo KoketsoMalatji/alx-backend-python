8c0ee20edacee30e3e6c31db02049ba16567d211
Koketso Malatji <koketsosean209@gmail.com>
2023-01-08 19:43:30 +0200

    python

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" string and int/float to tuple
+"""
+
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ Returns a tuple.
+    """
+    return (k, v**2)
