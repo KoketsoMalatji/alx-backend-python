9733e32bdcaf80f60960e468506ff227eeeddfc8
Koketso Malatji <koketsosean209@gmail.com>
2023-01-08 19:54:56 +0200

    python

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Duck typing
+"""
+
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ Gets first element safely.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
